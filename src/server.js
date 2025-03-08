d2a9b651a161e35baa4c6abf426a5f1d712814db
Bronny <bronnyhamilton@gmail.com>
2025-03-08 15:17:52 +1100

    feat: 404 route and error handler middleware

--- src/server.js
+++ src/server.js
@@ -8,6 +8,27 @@ app.get("/", (request, response) => {
   });
 });
 
+// wild card * means match any route
+// put this at the end of route declarations to catch anything that does no match an earlier route
+app.get("*", (request, response) => {
+  console.log("User tried to visit " + request.path);
+  response.json({
+    message: "Page not found.",
+    attemptedPath: request.path,
+  });
+});
+
+// error handling catcher applies to EVERY route in the entire server
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log("Error occurred in the server.");
+  console.log(JSON.stringify(error));
+  response.json({
+    errors: request.body.errors,
+    message: error.message,
+  });
+});
+
 module.exports = {
   app,
 };
